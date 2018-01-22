e596dde79488d1430d5aafa05f8a60a7c472d25d
charus120 <35423646+charus120@users.noreply.github.com>
2018-01-22 23:57:57 +0530

    Add files via upload

---
+++ sort1.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;   
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+int main()
+{
+int a[100000],i,m,n,key, j;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}  
+quickSort(a, 0, n-1); 
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+}
+return 0;
+}
