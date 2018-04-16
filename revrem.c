a3001a8b6be08a0eed7de17dff7d7d2e5863bdcc
charus120 <35423646+charus120@users.noreply.github.com>
2018-04-16 23:42:50 +0530

    Add files via upload
    print reverse of the string by removing the vowel

---
+++ revrem.c
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+
+#include<string.h>
+
+int main()
+
+{
+
+char a[100]={'\0'};
+
+int i;
+
+gets(a);
+
+for(i=strlen(a)-1;i>=0;i--)
+
+{
+
+if(a[i]!='a'&&a[i]!='e'&&a[i]!='i'&&a[i]!='o'&&a[i]!='u'&&a[i]!='A'&&a[i]!='E'&&a[i]!='I'&&a[i]!='O'&&a[i]!='U')
+
+{
+
+printf("%c",a[i]);
+
+}
+
+else
+
+continue;
+
+}
+
+return 0;
+
+}
